2d53edd6bdcd04aaa07e45dbe41307345de5ccdc
pedromshin <pedromaschio.shin@gmail.com>
2021-06-29 23:12:51 -0300

    n faço ideia do que estou fazendo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const App = () => {
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => {
+    return (
+      <div>
+      <h1>{props.course}</h1>
+      </div>
+    )
+  }
+  
+  const Part = (props) => {
+    return (
+      <div>
+        <p>
+          {props.part} {props.exercise}
+        </p>
+      </div>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} exercise={exercises1} />
+        <Part part={part2} exercise={exercises2} />
+        <Part part={part3} exercise={exercises3} />
+      </div>
+    )
+  }
+
+  const Total = () => {
+    return (
+      <div>
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
