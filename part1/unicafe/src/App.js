b678d945e7788b0d686938a82c1b9ce0806bf879
pedromshin <pedromaschio.shin@gmail.com>
2021-07-01 17:44:21 -0300

    fullstackopen exercise 1.8 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,27 @@
 import React, { useState } from "react";
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>
+        {props.GoodFeedback} {props.GoodCounter}
+      </p>
+      <p>
+        {props.NeutralFeedback} {props.NeutralCounter}
+      </p>
+      <p>
+        {props.BadFeedback} {props.BadCounter}
+      </p>
+      <p>
+        {props.textAll} {props.sumAll}
+      </p>
+      <p>Average {props.average}</p>
+      <p>Positive {props.percentage}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -7,7 +29,7 @@ const App = () => {
   const [bad, setBad] = useState(0);
   const [score, setScore] = useState(0);
   let total = good + neutral + bad;
-  const average = score / total;
+  let averageCalc = score / total;
 
   const increaseGoodByOne = () => {
     setGood(good + 1);
@@ -26,51 +48,24 @@ const App = () => {
     console.log("clicked bad");
   };
 
-  const StatsType = (props) => {
-    return (
-      <p>
-        {props.typeOfFeedback} {props.counter}
-      </p>
-    );
-  };
-
-  const SumAll = (props) => {
-    return (
-      <div>
-        <p>All {props.total}</p>
-      </div>
-    );
-  };
-
-  const Average = (props) => {
-    return (
-      <div>
-        <p>Average {props.average}</p>
-      </div>
-    );
-  };
-
-  const Percentage = (props) => {
-    return (
-      <div>
-        <p>Percentage {(props.good * 100) / props.total}%</p>
-      </div>
-    );
-  };
-
   return (
     <div>
       <h1>Give Feedback</h1>
       <button onClick={increaseGoodByOne}>Good</button>
       <button onClick={increaseNeutralByOne}>Neutral</button>
       <button onClick={increaseBadByOne}>Bad</button>
-      <h1>Statistics</h1>
-      <StatsType typeOfFeedback={"Good"} counter={good} />
-      <StatsType typeOfFeedback={"Neutral"} counter={neutral} />
-      <StatsType typeOfFeedback={"Bad"} counter={bad} />
-      <SumAll total={total} />
-      <Average average={average} />
-      <Percentage good={good} total={total} />
+      <Statistics
+        GoodFeedback={"Good"}
+        GoodCounter={good}
+        NeutralFeedback={"Neutral"}
+        NeutralCounter={neutral}
+        BadFeedback={"Bad"}
+        BadCounter={bad}
+        textAll={"All"}
+        sumAll={total}
+        average={averageCalc}
+        percentage={(good * 100) / total}
+      />
     </div>
   );
 };
